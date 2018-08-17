6ae989ea0eeee134f3ef2d840cb144d87d3ada9f
Jan Hartigan <janhartigan13@gmail.com>
2018-08-16 19:38:31 -0700

    getting closer to installing tensorflow

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import tensorflow
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	hello = tensorflow.constant('Hello, TensorFlow!')
+	sess = tensorflow.Session()
+	return sess.run(hello)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
---
+++ readme.md
@@ -0,0 +1,1 @@
+ImparaAI prediction app
