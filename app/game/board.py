1781806c8aec26cea0deabbe16bc0e24dff708bf
Jan Hartigan <janhartigan13@gmail.com>
2018-08-21 20:59:48 -0700

    some logic updates

--- app/game/board.py
+++ app/game/board.py
@@ -1,36 +1,43 @@
 import inspect
 from .piece import Piece
+from functools import reduce
 
 class Board:
 
 	player_turn = 1
-	rows = [
-		[Piece(1), Piece(1), Piece(1), Piece(1)],
-		[Piece(1), Piece(1), Piece(1), Piece(1)],
-		[Piece(1), Piece(1), Piece(1), Piece(1)],
-		[None, None, None, None],
-		[None, None, None, None],
-		[Piece(2), Piece(2), Piece(2), Piece(2)],
-		[Piece(2), Piece(2), Piece(2), Piece(2)],
-		[Piece(2), Piece(2), Piece(2), Piece(2)]
-	]
+	pieces = []
 
-	def set(self, state = None):
-		if state:
-			pass
+	def __init__(self):
+		set_starting_pieces()
+
+	def set_starting_pieces(self):
+		for i in range(1, 13):
+			piece = Piece()
+			piece.player = 1
+			piece.position = i
+			piece.board = self
+			self.pieces.push(piece)
+
+		for i in range(13, 33):
+			piece = Piece()
+			piece.player = 2
+			piece.position = i
+			piece.board = self
+			self.pieces.push(piece)
 
 	def count_player_pieces(player_number = 1):
-		count = 0
-
-		for row in self.rows:
-			for spot in row:
-				if isinstance(spot, Piece) and spot.player == player_number:
-					count += 1
-
-		return count
+		return reduce((lambda count, piece: count + (1 if piece.counts_for(player_number) else 0)), self.pieces)
 
 	def get_possible_moves(self):
-		pass
+		capture_moves = self.get_possible_capture_moves()
+
+		return capture_moves if capture_moves else self.get_possible_positional_moves()
+
+	def get_possible_capture_moves(self):
+		return reduce((lambda moves, piece: moves + piece.get_possible_capture_moves()), self.pieces)
+
+	def get_possible_positional_moves(self):
+		return reduce((lambda moves, piece: moves + piece.get_possible_positional_moves()), self.pieces)
 
 	def create_new_board_from_move(self, move):
 		pass
--- app/game/piece.py
+++ app/game/piece.py
@@ -1,6 +1,11 @@
 class Piece:
 	player = None
 	king = False
+	captured = False
+	position = None
 
 	def __init__(self, player):
 		self.player = player
+
+	def counts_for(self, player_number):
+		return self.player == player_number and not self.captured
