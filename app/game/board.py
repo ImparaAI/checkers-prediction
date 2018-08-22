ef7c3b0f61c5dca00fbd0f9c01e2d53d21ad2fcd
Jan Hartigan <janhartigan13@gmail.com>
2018-08-22 00:17:07 -0700

    general improvements and other wip

--- app/game/board.py
+++ app/game/board.py
@@ -5,14 +5,15 @@ from functools import reduce
 class Board(object):
 
 	player_turn = 1
-	pieces = []
-	open_spots = []
-	filled_spots = []
 	width = 4
 	height = 8
 	spot_count = width * height
 	rows_per_user_with_pieces = 3
 	spot_layout = {}
+	pieces = []
+	open_spots = []
+	filled_spots = []
+	player_spots = []
 
 	def __init__(self):
 		set_layout()
@@ -32,23 +33,21 @@ class Board(object):
 	def set_starting_pieces(self):
 		pieces = []
 		starting_piece_count = self.width * self.rows_per_user_with_pieces
-		first_player_starting_spots = range(1, starting_piece_count + 1)
-		second_player_starting_spots = range(self.spot_count - starting_piece_count + 1, self.spot_count + 1)
+		player_starting_spots = {
+			1: range(1, starting_piece_count + 1),
+			2: range(self.spot_count - starting_piece_count + 1, self.spot_count + 1)
+		}
 
 		for row in self.spot_layout:
 			for spot in row:
-				if spot in first_player_starting_spots:
-					player = 1
-				elif spot in second_player_starting_spots:
-					player = 2
-				else:
-					continue
+				player = 1 if spot in player_starting_spots[1] else 2 if spot in player_starting_spots[2] else None
 
-				piece = Piece()
-				piece.player = player
-				piece.position = spot
-				piece.board = self
-				pieces.push(piece)
+				if (player):
+					piece = Piece()
+					piece.player = player
+					piece.position = spot
+					piece.board = self
+					pieces.push(piece)
 
 		self.pieces = pieces
 
@@ -69,18 +68,30 @@ class Board(object):
 	def spot_is_open(self, spot):
 		return spot in self.open_spots
 
+	def get_piece_by_position(self, position):
+		for piece in self.pieces:
+			if piece.position == position:
+				return piece
+
 	def create_new_board_from_move(self, move):
 		pass
 
 	def update_piece_data(self):
 		self.update_filled_spots()
 		self.update_open_spots()
+		self.update_player_spots()
 
 	def update_filled_spots(self):
 		self.filled_spots = reduce((lambda open_spots, piece: open_spots + [piece.position]), self.pieces)
 
 	def update_open_spots(self):
 		self.open_spots = [spot for spot in range(1, self.spot_count) if not spot in self.filled_spots]
+
+	def update_player_spots(self):
+		self.player_spots = {
+			1: reduce((lambda spots, piece: spots + [piece.position] if piece.position == 1 else []), self.pieces)
+			2: reduce((lambda spots, piece: spots + [piece.position] if piece.position == 2 else []), self.pieces)
+		}
 
 	def __setattr__(self, name, value):
 		super(Board, self).__setattr__(name, value)
--- app/game/piece.py
+++ app/game/piece.py
@@ -1,7 +1,8 @@
-from functools import map
+from functools import filter, map
 
 class Piece(object):
 	player = None
+	other_player = None
 	king = False
 	captured = False
 	position = None
@@ -11,36 +12,43 @@ class Piece(object):
 		return self.player == player_number and not self.captured
 
 	def get_possible_capture_moves(self):
-		pass
+		adjacent_enemy_positions = filter((lambda position: position in self.board.player_spots[self.other_player]), self.get_adjacent_positions())
+
+		capture_move_positions = []
+
+		for enemy_position in adjacent_enemy_positions:
+			enemy_piece = self.board.get_piece_by_position(enemy_position)
+			position_behind_enemy = enemy_piece.
 
 	def get_possible_positional_moves(self):
-		new_positions = self.get_forward_adjacent_diagonal_positions()
-
-		if self.king:
-			new_positions += self.get_backward_adjacent_diagonal_positions()
+		adjacent_positions = filter((lambda position: self.board.spot_is_open(position)), self.get_adjacent_positions())
 
 		return list(map((lambda new_position: [self.position, new_position]), new_positions))
 
-	def get_forward_adjacent_diagonal_positions(self):
-		return self.get_adjacent_diagonal_positions()
+	def get_adjacent_positions(self):
+		return self.get_directional_adjacent_positions(forward = True) + (self.get_directional_adjacent_positions(forward = False) if self.king else [])
 
-	def get_backward_adjacent_diagonal_positions(self):
-		return self.get_adjacent_diagonal_positions(False)
-
-	def get_adjacent_diagonal_positions(self, forward = True):
+	def get_directional_adjacent_positions(self, forward):
 		positions = []
 		current_row = ceil(self.position / self.board.height)
 		current_column = (self.position - 1) % self.board.width
 		next_row = current_row + ((1 if self.player == 1 else -1) * (1 if forward else -1))
+		next_left_column = current_column - 1
+		next_right_column = current_column + 1
 
 		if not next_row in self.board.spot_layout:
 			return []
 
-		left_position = current_column - 1 if current_column - 1 > 0 else None
-		right_position = current_column + 1 if current_column + 1 < self.board.width else None
+		if next_left_column > 0:
+			position.push(self.board.spot_layout[next_row][next_left_column])
 
-		if self.board.spot_is_open(left_position):
-			positions.push(left_position)
+		if next_right_column < self.board.width:
+			position.push(self.board.spot_layout[next_row][next_right_column])
 
-		if self.board.spot_is_open(right_position):
-			positions.push(right_position)
+		return positions
+
+	def __setattr__(self, name, value):
+		super(self.__class__.__name__, self).__setattr__(name, value)
+
+		if name == 'player':
+			other_player = 1 if value == 2 else 2
