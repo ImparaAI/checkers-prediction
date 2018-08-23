8f00ea4bec7b40e00404b3466e8d0c8d75c6dbee
Jan Hartigan <janhartigan13@gmail.com>
2018-08-22 23:36:32 -0700

    fix class name

--- test/game/test_game.py
+++ test/game/test_game.py
@@ -1,7 +1,7 @@
 import unittest
 from app.game.game import Game
 
-class TestStringMethods(unittest.TestCase):
+class TestGame(unittest.TestCase):
 
 	def test_new_game_not_over(self):
 		self.assertEqual(Game().is_over(), False)
