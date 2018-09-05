7428e039c5aab3825984e565e4b72c106edefaa8
Jan Hartigan <janhartigan13@gmail.com>
2018-09-04 18:20:24 -0700

    starting to build the training session runner

--- app/training/session/runner.py
+++ app/training/session/runner.py
@@ -1,23 +1,52 @@
 from copy import deepcopy
+from model import builder
 from app.game.game import Game
 from app.montecarlo.node import Node
 from app.montecarlo.montecarlo import MonteCarlo
 
-def run():
+class Runner:
 
+	def __init__(self):
+		self.model = builder.build()
+		self.game = None
+		self.montecarlo = None
 
-def play_game():
-	montecarlo = MonteCarlo(Node(Game()))
-	montecarlo.child_finder = child_finder
+	def run(self):
+		for i in range(1000):
+			self.play_game()
 
-	for i in range(50):
-		montecarlo.simulate()
+		self.train()
 
-	chosen_node = montecarlo.make_choice()
-	self.assertIs(chosen_node.state, 1)
+	def play_game(self):
+		self.game = Game()
+		self.montecarlo = MonteCarlo(Node(self.game))
+		self.montecarlo.child_finder = self.child_finder
 
-def child_finder(node):
-	for move in node.state.get_possible_moves():
-		child = Node(deepcopy(node.state))
-		child.state.move(move)
-		node.add_child(child)
+		while not self.game.is_over():
+			self.play_turn()
+
+		self.store_game_training_data()
+
+	def play_turn(self):
+		for i in range(50):
+			montecarlo.simulate()
+
+		chosen_node = montecarlo.make_choice()
+		self.montecarlo.change_root_node(chosen_node)
+		self.game.move(chosen_node.state.moves[-1])
+
+	def child_finder(self, node):
+		prediction = self.model.predict(self.build_input(node.state))
+		node.update_win_value(prediction.win_value)
+
+		for move in node.state.get_possible_moves():
+			child = Node(deepcopy(node.state))
+			child.state.move(move)
+			child.policy_value = self.get_policy_value(child.state, prediction.policy_values)
+			node.add_child(child)
+
+	def build_input(self, game):
+		pass
+
+	def get_policy_value(self, game, policy_values):
+		pass
