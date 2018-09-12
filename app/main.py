d5122240852dcd202083f2cb61178412264d4dbd
Sergio Somma <sdsomma@gmail.com>
2018-09-12 11:56:06 -0700

    some fake move

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def train():
 
 @app.route("/predict", methods=['GET'])
 def predict():
-	return jsonify({'prediction': predictor.predict(request.json)})
+	return jsonify({'prediction': [23, 18]})
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
