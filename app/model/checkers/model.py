0ec0d446a7e6846c1b154249c6f850ed9bad00a8
Jan Hartigan <janhartigan13@gmail.com>
2019-01-04 00:58:09 -0800

    fixing saving training file

--- app/model/checkers/model.py
+++ app/model/checkers/model.py
@@ -3,7 +3,11 @@ from app.model.model import Model
 from app.training.session import fetcher as training_session_fetcher
 
 def build(model_name = None):
-	session = training_session_fetcher.get_latest_session() if not model_name else ''
-	filename = os.path.join(os.path.dirname(__file__), 'data/' + session['name'] + '.h5') if session else None
+	session = training_session_fetcher.get_latest_session() if not model_name else None
+	filename = None
 
+	if session or model_name:
+		filename = os.path.join(os.path.dirname(__file__), 'data/' + (model_name if model_name else session['name']) + '.h5')
+
+	print('weights file path', session, filename)
 	return Model(filename, (34, 8, 4), 8 * 8 * 4)
