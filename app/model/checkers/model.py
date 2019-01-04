38e6f54be82c24d1159d1bcc02acad7d080f5157
Jan Hartigan <janhartigan13@gmail.com>
2019-01-04 15:54:36 -0800

    adding weights directory under data to attempt to solve gke mounting problem

--- app/model/checkers/model.py
+++ app/model/checkers/model.py
@@ -7,6 +7,11 @@ def build(model_name = None):
 	filename = None
 
 	if session or model_name:
-		filename = os.path.join(os.path.dirname(__file__), 'data/' + (model_name if model_name else session['name']) + '.h5')
+		directory = os.path.join(os.path.dirname(__file__), 'data/weights/')
+
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+
+		filename = directory + (model_name if model_name else session['name']) + '.h5'
 
 	return Model(filename, (34, 8, 4), 8 * 8 * 4)
