dec6383c0ce8b8b5201e12af98bbdf81b7bc0ca1
Jan Hartigan <janhartigan13@gmail.com>
2019-01-04 20:34:45 -0800

    cleaning up the model loading

--- app/model/checkers/model.py
+++ app/model/checkers/model.py
@@ -4,14 +4,10 @@ from app.training.session import fetcher as training_session_fetcher
 
 def build(model_name = None):
 	session = training_session_fetcher.get_latest_session() if not model_name else None
-	filename = None
+	weights_path = None
 
 	if session or model_name:
-		directory = os.path.join(os.path.dirname(__file__), 'data/weights/')
+		filename = model_name if model_name else session['name'] + '.h5'
+		weights_path = os.path.join(os.path.dirname(__file__), 'data/' + filename)
 
-		if not os.path.exists(directory):
-			os.makedirs(directory)
-
-		filename = directory + (model_name if model_name else session['name']) + '.h5'
-
-	return Model(filename, (34, 8, 4), 8 * 8 * 4)
+	return Model(weights_path, (34, 8, 4), 8 * 8 * 4)
